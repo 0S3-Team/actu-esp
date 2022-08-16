265ae491f0e172efba3e3ea57b801f135edcfea1
Assane DIATTA <assanediatta96@gmail.com>
2022-08-16 22:59:49 +0000

    refact index

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
-include_once "views/admin/homeView.php";
 if (isset($_GET['users'])) {
   include_once "views/admin/listUserView.php";
 } elseif (isset($_GET['categories'])) {
   include_once "views/admin/listCategoryView.php";
 } elseif (isset($_GET['articles'])) {
-  include_once "views/admin/listCategoryView.php";
+  include_once "views/admin/listArticleView.php";
+} else {
+  include_once "views/admin/homeView.php";
 }
 ?>

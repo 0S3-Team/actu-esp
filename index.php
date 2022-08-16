c32a3f0da3001c7e47751e52e55623f62f50e665
Assane DIATTA <assanediatta96@gmail.com>
2022-08-16 22:56:51 +0000

    return views by query parameter

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
-include_once "views/admin/accueil.php";
+include_once "views/admin/homeView.php";
+if (isset($_GET['users'])) {
+  include_once "views/admin/listUserView.php";
+} elseif (isset($_GET['categories'])) {
+  include_once "views/admin/listCategoryView.php";
+} elseif (isset($_GET['articles'])) {
+  include_once "views/admin/listCategoryView.php";
+}
 ?>

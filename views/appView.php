b82423e79531dc87879b6e5c49d285fea04a0270
Assane DIATTA <assanediatta96@gmail.com>
2022-08-21 12:56:03 +0000

    update index.php

--- views/appView.php
+++ views/appView.php
@@ -1,8 +1,15 @@
 <?php
-
-    if (isset($_GET['blog'])) {
+    <?php
+    if (isset($_GET['list-users'])) {
+      $listUser // gnerer par le controleur
+      include_once "./admin/listUserView.php";
+    } elseif (isset($_GET['list-categories'])) {
+      include_once "./admin/listCategoryView.php";
+    } elseif (isset($_GET['list-articles'])) {
+      include_once "./admin/listArticleView.php";
+    } elseif (isset($_GET['blog'])) {
         include_once "./blog/homeView.php";
-    } else {
-        include_once "./admin/indexAdminView.php";
-    }
+    }  else {
+      include_once "./admin/homeView.php";
+    } 
 ?>

2cfa3ac1c612b18bee1c4d601281bb9807022c21
Assane DIATTA <assanediatta96@gmail.com>
2022-08-21 12:21:46 +0000

    new structure for views and add basic blog views

--- index.php
+++ index.php
@@ -1,11 +1,3 @@
 <?php
-if (isset($_GET['list-users'])) {
-  include_once "views/admin/listUserView.php";
-} elseif (isset($_GET['list-categories'])) {
-  include_once "views/admin/listCategoryView.php";
-} elseif (isset($_GET['list-articles'])) {
-  include_once "views/admin/listArticleView.php";
-} else {
-  include_once "views/admin/homeView.php";
-}
+    include_once "./views/appView.php";
 ?>
---
+++ views/admin/indexAdminView.php
@@ -0,0 +1,12 @@
+<?php
+if (isset($_GET['list-users'])) {
+  $listUser // gnerer par le controleur
+  include_once "views/admin/listUserView.php";
+} elseif (isset($_GET['list-categories'])) {
+  include_once "views/admin/listCategoryView.php";
+} elseif (isset($_GET['list-articles'])) {
+  include_once "views/admin/listArticleView.php";
+} else {
+  include_once "views/admin/homeView.php";
+}
+?>

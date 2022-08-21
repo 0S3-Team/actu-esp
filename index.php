0ff1389db2716582cff238079d630f28aab31345
Assane DIATTA <assanediatta96@gmail.com>
2022-08-21 13:01:27 +0000

    fix index.php bug

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
-    require "./views/appView.php";
+    if (isset($_GET['list-users'])) {
+      $listUser // gnerer par le controleur
+      include_once "./views/admin/listUserView.php";
+    } elseif (isset($_GET['list-categories'])) {
+      include_once "./views/admin/listCategoryView.php";
+    } elseif (isset($_GET['list-articles'])) {
+      include_once "./views/admin/listArticleView.php";
+    } elseif (isset($_GET['blog'])) {
+        include_once "./views/blog/homeView.php";
+    }  else {
+      include_once "./views/admin/homeView.php";
+    } 
 ?>
